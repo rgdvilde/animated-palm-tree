e1fd18064e41289bc20e40d734f15e7b3559bc38
rgdvilde <robbe.devilder@ugent.be>
2018-07-12 14:05:50 +0200

    project & clone pushed

---
+++ graphql/java_tests/grapqljava_mogodb/hackernews-graphql-java/src/main/java/com/howtographql/hackernews/Query.java
@@ -0,0 +1,17 @@
+package com.howtographql.hackernews;
+
+import java.util.List;
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+
+public class Query implements GraphQLRootResolver {
+    
+    private final LinkRepository linkRepository;
+
+    public Query(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    public List<Link> getAllLinks() {
+        return linkRepository.getAllLinks();
+    }
+}
---
+++ graphql/java_tests/grapqljava_postgres/hackernews-graphql-java/src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -0,0 +1,60 @@
+package com.howtographql.hackernews;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+import com.mongodb.client.model.*;
+
+public class LinkRepository {
+
+    private final MongoCollection<Document> links;
+
+    public LinkRepository(MongoCollection<Document> links) {
+        this.links = links;
+    }
+
+    public Link findById(String id) {
+        Document doc = links.find(eq("_id", new ObjectId(id))).first();
+        return link(doc);
+    }
+    
+    public List<Link> getAllLinks() {
+        List<Link> allLinks = new ArrayList<>();
+        for (Document doc : links.find()) {
+            allLinks.add(link(doc));
+        }
+        return allLinks;
+    }
+    
+    public void saveLink(Link link) {
+        Document doc = new Document();
+        doc.append("url", link.getUrl());
+        doc.append("description", link.getDescription());
+        links.insertOne(doc);
+    }
+    
+    public Link deleteLink(String url){
+    	Document d = links.findOneAndDelete(Filters.eq("url", url));
+    	return link(d);
+    }
+    
+    
+    
+    private Link link(Document doc) {
+        return new Link(
+                doc.get("_id").toString(),
+                doc.getString("url"),
+                doc.getString("description"));
+    }
+}
